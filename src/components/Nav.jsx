6b06302d9ac308701a4e20c8f95d507d55c03887
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-07 16:59:31 +0100

    added a folder for images

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,26 @@
 function Nav() {
-  return <nav></nav>;
+  return (
+    <nav>
+      <h1>
+        <span>TechTrove</span>
+        <span>Gadgets</span>
+      </h1>
+      <div>
+        <select>
+          <option value="categories">Categories</option>
+          <option value="categories">Phones</option>
+          <option value="categories">Laptop</option>
+          <option value="categories">Headset</option>
+        </select>
+        <input type="search" placeholder="Search Items" />
+      </div>
+      <div>
+        <img src="" alt="profile-icon" />
+        <img src="" alt="favourite-icon" />
+        <img src="" alt="cart-icon" />
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
