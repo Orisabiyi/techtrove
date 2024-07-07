8566403f66b0597098738bf2f9d0c3c27cc63be6
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-07 19:15:57 +0100

    complete the styling of the filter section

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,34 +1,35 @@
 import { NavLink } from "react-router-dom";
+import styles from "./Filter.module.css";
 
 function Filter() {
   return (
-    <ul>
+    <ul className={styles.list}>
       <li>
-        <NavLink>
+        <NavLink to="/">
           <img src="images/category-menu.svg" alt="menu-icon" />
           <span>Shop by category</span>
         </NavLink>
       </li>
       <li>
-        <NavLink>
+        <NavLink to="shop">
           <span>Shop</span>
           <img src="images/drop-down.svg" alt="dropdwon-icon" />
         </NavLink>
       </li>
       <li>
-        <NavLink>
+        <NavLink to="tag">
           <img src="images/tag.svg" alt="tag-icon" />
           <span>Special prices</span>
         </NavLink>
       </li>
       <li>
-        <NavLink>
+        <NavLink to="deals">
           <img src="images/deals.svg" alt="deals icon" />
           <span>Deals today</span>
         </NavLink>
       </li>
       <li>
-        <NavLink>
+        <NavLink to="history">
           <img src="images/history.svg" alt="history-icon" />
           <span>History</span>
         </NavLink>
