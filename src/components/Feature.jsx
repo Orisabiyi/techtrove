416c5fa55dd851e1d0fa184e6eef5b3622c80a2f
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-08 04:52:53 +0100

    add brand images

---
+++ src/components/Feature.jsx
@@ -0,0 +1,20 @@
+import styles from "./Feature.module.css";
+
+function Feature() {
+  return (
+    <section className={styles.feature}>
+      <div>
+        <h2>Featured brands</h2>
+        <p>See all brands</p>
+      </div>
+
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </section>
+  );
+}
+
+export default Feature;
