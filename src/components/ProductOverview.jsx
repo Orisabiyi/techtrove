cd2360b66a5b7f11f45787489fccd9e322ff0588
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-08 18:24:23 +0100

    include the jsx for product

--- src/components/ProductOverview.jsx
+++ src/components/ProductOverview.jsx
@@ -1,8 +1,21 @@
 import styles from "./ProductOverview.module.css";
 
 function ProductOverview() {
-  const items = Array.from({ length: 10 }, function (_, index) {
-    return <div key={index}>Item {index + 1}</div>;
+  const items = Array.from({ length: 9 }, function (_, index) {
+    return (
+      <div key={index}>
+        <div>
+          <img src="images/oraimo.svg" alt="oraimo-icon" />
+        </div>
+        <h3>Oraimo airpods</h3>
+        <p className={styles.description}>
+          <span>Description</span>
+          <img src="images/like.svg" alt="like-icon" />
+        </p>
+        <p className={styles.price}># 11, 900.00</p>
+        <button>Add to cart</button>
+      </div>
+    );
   });
 
   return (
