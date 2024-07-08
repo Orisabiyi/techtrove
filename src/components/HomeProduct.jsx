ad8fb3d40354cb39539fc1fe71dce32d80ff89b8
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-08 05:28:28 +0100

    added images for product

---
+++ src/components/HomeProduct.jsx
@@ -0,0 +1,12 @@
+import styles from "./HomeProduct.module.css";
+
+function HomeProduct() {
+  return (
+    <section className={styles.product}>
+      <div></div>
+      <ul></ul>
+    </section>
+  );
+}
+
+export default HomeProduct;
