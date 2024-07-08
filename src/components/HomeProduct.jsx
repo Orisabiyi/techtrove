4fc3d936823d67e525dccaf81046649b05cf5e00
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-08 05:48:19 +0100

    Homeproduct jsx

--- src/components/HomeProduct.jsx
+++ src/components/HomeProduct.jsx
@@ -3,8 +3,36 @@ import styles from "./HomeProduct.module.css";
 function HomeProduct() {
   return (
     <section className={styles.product}>
-      <div></div>
-      <ul></ul>
+      <div>
+        <h2>Hot Selling</h2>
+        <p>See all</p>
+      </div>
+      <ul>
+        <li>
+          <img src="images/oraimo.svg" alt="oraimo-icon" />
+          <p>Oraimo airpods</p>
+          <p># 11, 900.00</p>
+          <button>Add to cart</button>
+        </li>
+        <li>
+          <img src="images/oraimo.svg" alt="oraimo-icon" />
+          <p>Oraimo airpods</p>
+          <p># 11, 900.00</p>
+          <button>Add to cart</button>
+        </li>
+        <li>
+          <img src="images/oraimo.svg" alt="oraimo-icon" />
+          <p>Oraimo airpods</p>
+          <p># 11, 900.00</p>
+          <button>Add to cart</button>
+        </li>
+        <li>
+          <img src="images/oraimo.svg" alt="oraimo-icon" />
+          <p>Oraimo airpods</p>
+          <p># 11, 900.00</p>
+          <button>Add to cart</button>
+        </li>
+      </ul>
     </section>
   );
 }
