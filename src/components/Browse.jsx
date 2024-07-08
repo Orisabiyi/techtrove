32fd8004c1f7cf553042da3f23d546bbd17920c8
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-08 04:45:04 +0100

    finish styling the browse section

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -10,18 +10,23 @@ function Browse() {
       <ul>
         <li>
           <img src="images/browse-phone.svg" alt="phone-icon" />
+          <p>Phones</p>
         </li>
         <li>
           <img src="images/browse-headset.svg" alt="headset-icon" />
+          <p>Accessories</p>
         </li>
         <li>
           <img src="images/browse-computer.svg" alt="computer-icon" />
+          <p>Computers</p>
         </li>
         <li>
           <img src="images/browse-lamp.svg" alt="lamp-icon" />
+          <p>Appliances</p>
         </li>
         <li>
           <img src="images/browse-tv.svg" alt="tv-icon" />
+          <p>TVs</p>
         </li>
       </ul>
     </section>
