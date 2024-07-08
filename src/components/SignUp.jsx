cd9eb0e72533b8817d823ec8657dde3f31a8d947
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-08 06:25:33 +0100

    Signup jsx

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,26 @@
+import styles from "./SignUp.module.css";
+
+function SignUp() {
+  return (
+    <section className={styles.signup}>
+      <div>
+        <div>
+          <h2>Get 25% off</h2>
+          <p>When you sign up and shop with us for the first time</p>
+
+          <form action="#" method="post">
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm Password" />
+            <button type="submit">Sign up</button>
+          </form>
+        </div>
+        <div>
+          <img src="images/projector.svg" alt="projector-icon" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default SignUp;
