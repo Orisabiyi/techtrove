d139e286f714c1ae5ae388cde5610dc9c33ee5c1
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-09 02:33:10 +0100

    implement dropdown for the filter component

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,30 +1,58 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Filter.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Filter() {
   const [dropDown, setDropDown] = useState(false);
+  const [navName, setNavName] = useState("Home");
 
   const toggleDropdown = function () {
     setDropDown((value) => !value);
   };
 
+  useEffect(
+    function () {
+      if (window.location.pathname.includes("/product")) setNavName("Product");
+      else setNavName("Home");
+    },
+    [navName]
+  );
+
   return (
     <ul className={styles.list}>
-      <li onClick={toggleDropdown}>
-        <NavLink to="/">
-          <span>Home</span>
-          <img src="images/drop-down.svg" alt="dropdwon-icon" />
-        </NavLink>
+      {navName === "Home" && (
+        <li onClick={toggleDropdown}>
+          <NavLink to="/">
+            <span>Home</span>
+            <img src="images/drop-down.svg" alt="dropdwon-icon" />
+          </NavLink>
 
-        {dropDown && (
-          <ul className={styles.dropdown}>
-            <li>
-              <NavLink to="product">Product</NavLink>
-            </li>
-          </ul>
-        )}
-      </li>
+          {dropDown && (
+            <ul className={styles.dropdown}>
+              <li>
+                <NavLink to="product">Product</NavLink>
+              </li>
+            </ul>
+          )}
+        </li>
+      )}
+
+      {navName === "Product" && (
+        <li onClick={toggleDropdown}>
+          <NavLink to={navName !== "Product" && "product"}>
+            <span>Product</span>
+            <img src="images/drop-down.svg" alt="dropdwon-icon" />
+          </NavLink>
+
+          {dropDown && (
+            <ul className={styles.dropdown}>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+            </ul>
+          )}
+        </li>
+      )}
       <li>
         <NavLink to="tag">
           <img src="images/tag.svg" alt="tag-icon" />
