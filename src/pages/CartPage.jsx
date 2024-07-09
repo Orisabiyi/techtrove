a738d8cf78ff0b79dd65c19a021bf71afe231a18
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-09 17:58:55 +0100

    include footer to the cart page

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,11 +1,13 @@
 import Cart from "../components/Cart";
 import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 function CartPage() {
   return (
     <section>
       <Nav />
       <Cart />
+      <Footer />
     </section>
   );
 }
